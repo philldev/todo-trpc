42f296b8d781f4b1004467dcb6b1e6c92deb5860
deddywolley <deddywolley@gmail.com>
2022-06-25 17:58:30 +0800

    added prisma

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,19 +1,30 @@
 import * as trpc from '@trpc/server'
 import * as trpcNext from '@trpc/server/adapters/next'
 import { z } from 'zod'
+import prisma from 'lib/prisma'
 
-export const appRouter = trpc.router().query('hello', {
-	input: z
-		.object({
-			text: z.string().nullish(),
-		})
-		.nullish(),
-	resolve({ input }) {
-		return {
-			greeting: `hello ${input?.text ?? 'world'}`,
-		}
-	},
-})
+export const appRouter = trpc
+	.router()
+	.mutation('addTodo', {
+		input: z.object({
+			text: z.string(),
+		}),
+		async resolve({ input }) {
+			await prisma.todo.create({
+				data: {
+					text: input.text,
+				},
+			})
+		},
+	})
+	.query('getTodos', {
+		async resolve() {
+			const todos = await prisma.todo.findMany()
+			return {
+				todos,
+			}
+		},
+	})
 
 // export type definition of API
 export type AppRouter = typeof appRouter
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,56 @@
 import type { NextPage } from 'next'
+import { useState } from 'react'
+import { trpc } from 'utils/trpc'
 
 const Home: NextPage = () => {
-	return <div></div>
+	return (
+		<div className='w-full min-h-screen overflow-x-hidden bg-gray-900 text-gray-50'>
+			<div className='p-4 text-4xl font-bold text-center'>TODO TRPC</div>
+			<div className='p-4'>
+				<AddTodo />
+				<Todos />
+			</div>
+		</div>
+	)
+}
+
+const AddTodo = () => {
+	const [text, setText] = useState('')
+	const { mutate } = trpc.useMutation('addTodo')
+
+	return (
+		<form
+			onSubmit={(e) => {
+				e.preventDefault()
+				if (text.trim().length > 0) {
+					mutate({
+						text,
+					})
+				}
+			}}
+		>
+			<input
+				className='w-full p-0 bg-transparent focus:outline-none'
+				placeholder='Add Todo...'
+				value={text}
+				onChange={(e) => setText(e.target.value)}
+			/>
+		</form>
+	)
+}
+
+const Todos = () => {
+	const { data, status } = trpc.useQuery(['getTodos'])
+
+	if (status === 'error' || status === 'loading') return <div>...Loading</div>
+
+	return (
+		<ul>
+			{data?.todos.map((todo) => {
+				return <li key={todo.id}>{todo.text}</li>
+			})}
+		</ul>
+	)
 }
 
 export default Home
